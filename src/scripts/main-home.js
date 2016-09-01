594f6595f9e9a248b56f143d30cec71dada63ae5
Pierre Reimertz <pierre@pierrereimertz.com>
2016-09-01 21:57:32 +0200

    night mode

--- src/scripts/main-home.js
+++ src/scripts/main-home.js
@@ -1,3 +1,7 @@
+require('es6-promise').polyfill()
+require('isomorphic-fetch')
+
+
 import Writer from './modules/Writer';
 import Translater from './modules/Translater';
 import RickRolled from './modules/RickRolled';
--- src/scripts/modules/NightMode.js
+++ src/scripts/modules/NightMode.js
@@ -1,6 +1,8 @@
 //npm install suncalc --save
 import * as SunCalc from 'suncalc'
 
+const html = document.querySelector('html')
+
 fetch('http://ip-api.com/json')
   .then((response) => {
     return response.json()
@@ -9,8 +11,8 @@ fetch('http://ip-api.com/json')
     const now = +new Date()
     const sunset = +new Date(SunCalc.getTimes(+new Date(), json.lat, json.lon).sunset)
     if(now > sunset) {
-      const classList = document.querySelector('html').getAttribute('class')
-      document.querySelector('html').setAttribute('class', `${classList} inverted`)
+      const classList = html.getAttribute('class')
+                        html.setAttribute('class', `${classList} inverted`)
     }
   })
 
